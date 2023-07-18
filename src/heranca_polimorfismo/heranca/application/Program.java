4796997b4c069d3468313192d486c780ebc5e66c
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-07-18 20:16:36 -0300

    Exercicio herança

---
+++ src/heranca_polimorfismo/heranca/application/Program.java
@@ -0,0 +1,39 @@
+package heranca_polimorfismo.heranca.application;
+
+import heranca_polimorfismo.heranca.model.entities.Account;
+import heranca_polimorfismo.heranca.model.entities.BusinessAccount;
+import heranca_polimorfismo.heranca.model.entities.SavingsAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Account acc = new Account(1001,"Alex",0.0);
+        BusinessAccount bacc = new BusinessAccount(1002,"Maria", 0.0, 500.0);
+
+        //UPCASTING
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003,"Bob", 0.0, 200.0);
+        Account acc3 = new SavingsAccount(1004,"Ana", 0.0, 0.01);
+
+
+        //DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+
+        //BusinessAccount acc5 = (BusinessAccount) acc3; - Erro porque acc3 foi instanciado como SavingsAccount
+        //Evitando erro de downcasting
+        if (acc3 instanceof BusinessAccount){ //Metodo instanceof utilizado para testar se acc3 é instancia de BusinessAccount
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+        if (acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
+    }
+}
