767d1f2268fddfccfa9fb228dc22bdec3a64438d
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-07-19 19:58:05 -0300

    Exercicio exceções

---
+++ src/tratamentoDeExcecoes/exception/application/Program.java
@@ -0,0 +1,48 @@
+package tratamentoDeExcecoes.exception.application;
+
+import tratamentoDeExcecoes.exception.model.entities.Reservation;
+import tratamentoDeExcecoes.exception.model.exceptions.DomainException;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Scanner in = new Scanner(System.in);
+
+        try {
+            System.out.print("Room Number: ");
+            int roomNumber = in.nextInt();
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            LocalDate checkIn = LocalDate.parse(in.next(), dtf);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            LocalDate checkOut = LocalDate.parse(in.next(), dtf);
+
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println(reservation);
+            System.out.println();
+
+            System.out.println("Enter data to update the reservation:");
+            System.out.print("Check-in date (dd/MM/yyyy): ");
+            checkIn = LocalDate.parse(in.next(), dtf);
+            System.out.print("Check-out date (dd/MM/yyyy): ");
+            checkOut = LocalDate.parse(in.next(), dtf);
+
+            reservation.updateDates(checkIn, checkOut);
+            System.out.println(reservation);
+        }
+        catch (DateTimeParseException e){
+            System.out.println("Invalid date format");
+        }
+        catch (DomainException e){
+            System.out.println(e.getMessage());
+        }
+        catch (RuntimeException e){
+            System.out.println("Unexpected error");
+        }
+        in.close();
+    }
+}
---
+++ src/tratamentoDeExcecoes/exception/model/entities/Reservation.java
@@ -0,0 +1,69 @@
+package tratamentoDeExcecoes.exception.model.entities;
+
+import tratamentoDeExcecoes.exception.model.exceptions.DomainException;
+
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+public class Reservation {
+
+    private Integer roomNumber;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+
+    public static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer roomNumber, LocalDate checkIn, LocalDate checkOut){
+        if (!checkOut.isAfter(checkIn)){
+            throw new DomainException("Error in reservation: Check-Out date must be after check-in date!");
+        }
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public long duration(){
+        Period diff = Period.between(checkIn, checkOut);
+        return diff.get(ChronoUnit.DAYS);
+    }
+
+    public void updateDates(LocalDate checkIn, LocalDate checkOut){
+        LocalDate now = LocalDate.now();
+        if (checkIn.isBefore(now) || checkOut.isBefore(now) ){
+            throw new DomainException("Error in reservation: Reservation dates for update must be future dates!");
+        }
+        if (!checkOut.isAfter(checkIn)){
+            throw new DomainException("Error in reservation: Check-Out date must be after check-in date!");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Reservation: ");
+        sb.append("Room ").append(roomNumber);
+        sb.append(", check-In: ").append(checkIn.format(dtf));
+        sb.append(", check-Out: ").append(checkOut.format(dtf));
+        sb.append(", ").append(duration()).append(" nights.");
+        return sb.toString();
+    }
+}
