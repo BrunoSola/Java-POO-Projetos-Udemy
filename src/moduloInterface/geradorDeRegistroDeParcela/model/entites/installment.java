ea16fbdee344babfc994c5041673354457243a2c
Bruno dos Reis Sola <brunosola3@gmail.com>
2023-10-05 21:29:43 -0300

    rename class

--- src/moduloInterface/geradorDeRegistroDeParcela/model/entites/installment.java
+++
@@ -1,9 +0,0 @@
-package moduloInterface.geradorDeRegistroDeParcela.model.entites;
-
-import java.time.LocalDate;
-
-public class installment {
-    private LocalDate dueDate;
-    private Double amount;
-
-}
